177e455016209369114386a95946cfa30d7aefee
8Sulu <120654733+8Sulu@users.noreply.github.com>
2023-11-21 03:16:28 -0500

    Update Final.cpp

--- Final.cpp
+++ Final.cpp
@@ -1,38 +1,41 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-const int THRESHOLD = 10;
+const int THRESHOLD = 100;
 
-// Function to calculate the sum of elements in an array
 int calculateSum(int arr[], int size) {
-
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum;
 }
 
 int main() {
     const int arraySize = 5;
     int numbers[arraySize];
 
-    // Populate the array with user input
     cout << "Enter " << arraySize << " numbers:" << endl;
+
     for (int i = 0; i < arraySize; ++i) {
         cout << "Number " << i + 1 << ": ";
-        cin >> numbers[i];
-
-        /*
-        if (invalid input) {
+        while (!(cin >> numbers[i])) {
             cout << "Invalid input. Please enter a number: ";
-            cin >> numbers[i];
-        } 
-        */
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
     }
 
-    // Calculate the sum using the function
     int result = calculateSum(numbers, arraySize);
 
-    // If the sum is greater than the threshold, print a message
+    cout << "Sum of the numbers: " << result << endl;
+
     if (result > THRESHOLD) {
-    
+        cout << "The sum is greater than the threshold." << endl;
+    } else {
+        cout << "The sum is not greater than the threshold." << endl;
     }
 
     return 0;
